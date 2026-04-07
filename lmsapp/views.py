47180346ade2410409804fce5d3da2648e50fbd6
s-h-m12 <mark-hm1@yandex.ru>
2026-04-07 11:54:07 +0700

    добавлена аутентификация с логированием

--- lmsapp/views.py
+++ lmsapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import UserLoginLog
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            UserLoginLog.objects.create(user=user)
+            return redirect('home')
+        else:
+            return render(request,'login.html',{'error': 'Некорректные данные'})
+    return render(request, 'login.html')
